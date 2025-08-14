1d7bbfe443b15749bb245bfa24b0ca24c928b67d
sourav-jyoti <souravjyoti64@gmail.com>
2025-08-14 14:12:36 +0530

    wrote 70% db.ts code

--- 4_second_brain/backend/src/db.ts
+++ 4_second_brain/backend/src/db.ts
@@ -1,13 +1,79 @@
-import mongoose, {model,Schema} from "mongoose";
+import mongoose,{model,Schema} from "mongoose";
 
-mongoose.connect("mongodb+srv://vdtube:1234@vdtube.n7i5bwd.mongodb.net/?retryWrites=true&w=majority&appName=vDtube")
+mongoose.connect("mongodb://localhost:27017/brainly")
+
+//creating schema
+
+const UserSchema = new Schema(
+    {
+        username:{
+            type:String,
+            required:true,
+            unique:true,
+            trim:true
+        },
+        password:{
+            type:String,
+            required:true
+        }
+        
+    }
+)
+
+const ContentSchema = new Schema(
+    {
+        //every user has there own content
+        userId:{
+            type:Schema.Types.ObjectId,
+            ref:"User",
+            required:true
+        },
+        link:{
+            type:String,
+            required:true,
+        },      
+        title:{
+            type:String,
+            required:true,
+        },
+        tags:[{
+            type:Schema.Types.ObjectId,
+            ref:"Tag"
+        }]
+        //square bracket tells it is an array
+    }
+)
 
 
-const UserSchema = new Schema({
-    username :{type:String,unique:true},
-    password:String
-})
+const TagSchema = new Schema(
+    {
+        title:{
+            type:String
+        }
+    }
+)
+
+
+const LinkSchema = new Schema(
+    {
+        
+    }
+)
+
+
+//creating model
 
 export const UserModel = model("User",UserSchema);
+export const ContentModel = model("Content",ContentSchema);
+export const TagModel = model("Tag",TagSchema);
+export const LinkModel = model("Link",LinkSchema);
+
+
+
+//==nect update 
+/**
+ **make the db connection inside try catch
+ **use TS genirics to achieve true Ts in db.ts
+ */
 
 
