a5d60b837136231e32afb24f1a099692091a25e4
sourav-jyoti <souravjyoti64@gmail.com>
2025-09-16 09:45:06 +0530

    some changes in ws-backend

--- 5_drawyy/apps/ws-backend/src/index.ts
+++ 5_drawyy/apps/ws-backend/src/index.ts
@@ -1,9 +1,32 @@
-// import { WebSocketServer } from "ws";
-// import dotenv from "dotenv";
-// dotenv.config();
-// import { userMiddleware } from "@repo/jsonwebtoken-verify/middleware";
+import dotenv from "dotenv";
+dotenv.config();
 
-// const wss = new WebSocketServer({ port: 8080 });
+//we can't use the jsonwebtoken-verify/middleware here , the way we use middleware like calling next() in express specific -- is not a syntax applicable in websockets
+import { WebSocket, WebSocketServer } from "ws";
+import jwt from "jsonwebtoken";
+import type { JwtPayload } from "jsonwebtoken"; //This way JwtPayload is treated as a type only, no runtime error.
+import { prismaClient } from "@repo/db/client";
+
+//jwtHandler
+
+function jwtmiddleware(token: string): string | null {
+   try {
+      if (!token || Array.isArray(token)) {
+         return null;
+      }
+      const decoded = jwt.verify(token, process.env.JWT_PASS) as JwtPayload;
+
+      if (!decoded.userId) {
+         return null;
+      }
+
+      return decoded.userId;
+   } catch (e) {
+      return "error decoding";
+   }
+}
+
+const wss = new WebSocketServer({ port: 8080 });
 
 // interface User {
 //    ws: WebSocket;
